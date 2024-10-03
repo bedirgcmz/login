d2f88489b4c814a45c5abdfac6f13da3e89a2a6d
bedirgcmz <bdrgcmz@gmail.com>
2024-10-03 10:02:22 +0200

    Added filter to category

--- src/app/category/[category]/page.tsx
+++ src/app/category/[category]/page.tsx
@@ -5,13 +5,15 @@ import { useUserContext } from "@/newcontext/AuthContext";
 import { userContextType } from "@/utils/types";
 import Swal from "sweetalert2";
 import Link from "next/link";
+import { FaFilter } from "react-icons/fa6";
 
 const CategoryPage = ({ params }: { params: { category: string } }) => {
   const userContext = useUserContext();
-  const { loggedIn, setLoggedIn, user, setUser } = userContext as userContextType;
+  const { user } = userContext as userContextType;
   const [meals, setMeals] = useState<any[]>([]);
   const [visibleCount, setVisibleCount] = useState<number>(6);
   const [isMoreAvailable, setIsMoreAvailable] = useState<boolean>(true);
+  const [filterText, setFilterText] = useState<string>("");
 
   useEffect(() => {
     const fetchMeals = async () => {
@@ -65,8 +67,6 @@ const CategoryPage = ({ params }: { params: { category: string } }) => {
         Swal.fire("You already added this to your favorites list!");
       }
     }
-
-    console.log(registeredUser);
   };
 
   return (
@@ -78,35 +78,62 @@ const CategoryPage = ({ params }: { params: { category: string } }) => {
         You are viewing the {params.category.charAt(0).toUpperCase() + params.category.slice(1)}{" "}
         category.
       </p>
+      {/* Arama çubuğu ve butonlar */}
+      <div className="flex flex-col md:flex-row items-center justify-center mb-8 gap-4">
+        <div className="w-full md:w-[400px] max-w-[400px] flex justify-center relative">
+          <input
+            type="text"
+            placeholder="Search for a meal..."
+            value={filterText}
+            onChange={(e) => setFilterText(e.target.value)}
+            className="px-4 pr-6 border w-full h-[41px] border-gray-300 rounded-l-lg outline-none"
+          />
+          <span
+            onClick={() => setFilterText("")}
+            className="absolute right-[58px] top-[7px] text-[#ccc] cursor-pointer"
+          >
+            x
+          </span>
+          <button className="px-4 py-2 h-[40px] bg-[#ddb892] hover:bg-[#b08968] text-white rounded-r-lg flex items-center justify-center">
+            <FaFilter />
+          </button>
+        </div>
+      </div>
 
       {/* Kartları içeren alan */}
       <div className="flex flex-wrap justify-center items-center gap-6">
-        {meals.slice(0, visibleCount).map((meal) => (
-          <div
-            key={meal.idMeal}
-            className="w-full md:w-[300px] h-[340px] flex flex-col p-4 border-[#e6ccb2] border-2 rounded-lg shadow-lg hover:shadow-2xl transition-shadow"
-          >
-            <img
-              src={meal.strMealThumb}
-              alt={meal.strMeal}
-              className="w-full h-48 object-cover rounded-lg mb-4"
-            />
-            <h3 className="text-xl font-semibold mb-2">
-              {meal.strMeal.length > 40 ? meal.strMeal.slice(0, 40) + "..." : meal.strMeal}
-            </h3>
-            <div className="flex justify-between items-center mt-auto">
-              <Link href={`/recipe/${meal.idMeal}`} className="text-[#b08968] hover:text-[#7f5539]">
-                Recipe
-              </Link>
-              <button
-                onClick={() => addMealToFavoriList(meal.idMeal)}
-                className="text-green-500 hover:text-green-700"
-              >
-                Save
-              </button>
+        {meals
+          .filter((meal) => meal.strMeal?.toLowerCase().includes(filterText.toLowerCase()))
+          .slice(0, visibleCount)
+          .map((meal) => (
+            <div
+              key={meal.idMeal}
+              className="w-full md:w-[300px] h-[340px] flex flex-col p-4 border-[#e6ccb2] border-2 rounded-lg shadow-lg hover:shadow-2xl transition-shadow"
+            >
+              <img
+                src={meal.strMealThumb}
+                alt={meal.strMeal}
+                className="w-full h-48 object-cover rounded-lg mb-4"
+              />
+              <h3 className="text-xl font-semibold mb-2">
+                {meal.strMeal.length > 40 ? meal.strMeal.slice(0, 40) + "..." : meal.strMeal}
+              </h3>
+              <div className="flex justify-between items-center mt-auto">
+                <Link
+                  href={`/recipe/${meal.idMeal}`}
+                  className="text-[#b08968] hover:text-[#7f5539]"
+                >
+                  Recipe
+                </Link>
+                <button
+                  onClick={() => addMealToFavoriList(meal.idMeal)}
+                  className="text-green-500 hover:text-green-700"
+                >
+                  Save
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
 
       {/* "See More" butonu ve uyarı mesajı */}
